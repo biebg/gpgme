e9538779d7580d513f33e58d81e5f2a21fcf6146
yuansc <823374796@qq.com>
2014-09-04 17:48:32 +0800

    update

--- gpgme.js
+++ gpgme.js
@@ -3,6 +3,3 @@ var fs = require('fs');
 exports.Sign = gpgme.Sign;
 exports.Verify = gpgme.Verify;
 exports.Export = gpgme.Export
-gpgme.Sign('yuansc', "hello", function (err, result) {
-	console.log(err, result);
-})
---
+++ readme.md
@@ -0,0 +1,5 @@
+##Gpgme
+
+###TODO
+* 完成gpgme lib在不同机器上的适配
+* 解决 Bad passphrase 问题
