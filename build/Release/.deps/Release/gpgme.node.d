ae099f21ab5fbbabed56c982e466725ccf81fc43
yuansc <823374796@qq.com>
2014-09-05 10:24:12 +0800

    update

--- build/Release/.deps/Release/gpgme.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/gpgme.node := ./gyp-mac-tool flock ./Release/linker.lock c++ -bundle -Wl,-search_paths_first -mmacosx-version-min=10.5 -arch x86_64 -L./Release  -o Release/gpgme.node Release/obj.target/gpgme/gpgme.o -undefined dynamic_lookup /usr//local/lib/libgpgme.dylib
